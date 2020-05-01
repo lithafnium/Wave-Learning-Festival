311f1c9ceb00c9f42278c602b9635395b9a4dbc7
Billy Billy <eptgen@gmail.com>
2020-05-01 01:08:38 -0700

    created links to team page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import About from './pages/About'
 import Teachers from './pages/Teachers'
+import Team from './pages/Team'
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
 
@@ -14,10 +15,13 @@ function App() {
           <Route path="/teachers">
             <Teachers />
           </Route>
+          <Route path="/team">
+            <Team />
+          </Route>
           <Route path="/">
             <About />
           </Route>
-          
+
 
         </Switch>
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,7 +8,7 @@ import {
     NavItem,
     Hamburger,
     Bar,
-    SideBar, 
+    SideBar,
     Brand
 } from './styles'
 import {
@@ -33,7 +33,7 @@ const Navbar = () => {
                         <Link to='/'>Home</Link>
                     </NavItem>
                     <NavItem>
-                        <Link to='/'>Team</Link>
+                        <Link to='/team'>Team</Link>
                     </NavItem>
                     <NavItem>
                         <Link to='/teachers'>Teachers</Link>
