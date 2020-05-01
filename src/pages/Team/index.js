6345210655b7f04022d912da4f7ef92618a0225e
dshuman12 <30298537+dshuman12@users.noreply.github.com>
2020-05-01 14:54:21 -0400

    Added school name + year

--- src/pages/Team/index.js
+++ src/pages/Team/index.js
@@ -29,16 +29,19 @@ const Team = () => {
               <div class="team-member">
                 <img src={KarlyHou} />
                 <p class="name">Karly Hou</p>
+                <p class="school">Harvard University, 2023</p>
                 <p class="desc">Chair</p>
               </div>
               <div class="team-member">
                 <img src={YaelLivneh}/>
                 <p class="name">Yael Livneh</p>
+                <p class="school">Cal Poly SLO, 2023</p>
                 <p class="desc">Director of Logistics</p>
               </div>
               <div class="team-member">
                 <img src={SalmaKamni} />
                 <p class="name">Salma Kamni</p>
+                <p class="school">Stanford University, 2024</p>
                 <p class="desc">Associate Director of Logistics</p>
               </div>
             </div>
@@ -47,16 +50,19 @@ const Team = () => {
               <div class="team-member">
                 <img src={SimonTao} />
                 <p class="name">Simon Tao</p>
+                <p class="school">Stanford University, 2022</p>
                 <p class="desc">Associate Director of Logistics</p>
               </div>
               <div class="team-member">
                 <img src={KevinTan} />
                 <p class="name">Kevin Tan</p>
+                <p class="school">Harvard University, 2023</p>
                 <p class="desc">Associate Director of Logistics</p>
               </div>
               <div class="team-member">
                 <img src={SteveLi} />
                 <p class="name">Steve Li</p>
+                <p class="school">Harvard University, 2023</p>
                 <p class="desc">Co-Director of Tech</p>
               </div>
             </div>
@@ -65,16 +71,19 @@ const Team = () => {
               <div class="team-member">
                 <img src={DanielaShuman} />
                 <p class="name">Daniela Shuman</p>
+                <p class="school">Harvard University, 2023</p>
                 <p class="desc">Co-Director of Tech</p>
               </div>
               <div class="team-member">
                 <img src={JonnyRogers} />
                 <p class="name">Jonny Rogers</p>
+                <p class="school">Williams College, 2023</p>
                 <p class="desc">Associate Director of Tech</p>
               </div>
               <div class="team-member">
                 <img src={IrisSu} />
                 <p class="name">Iris Su</p>
+                <p class="school">Harvard University, 2023</p>
                 <p class="desc">Director of PR</p>
               </div>
             </div>
@@ -83,16 +92,19 @@ const Team = () => {
               <div class="team-member">
                 <img src={RuhiMistry} />
                 <p class="name">Ruhi Mistry</p>
+                <p class="school">UC Riverside, 2024</p>
                 <p class="desc">Associate Director</p>
               </div>
               <div class="team-member">
                 <img src={NikkiSuzani} />
                 <p class="name">Nikki Suzani</p>
+                <p class="school">Henry M. Gunn High School, 2021</p>
                 <p class="desc">Associate Director</p>
               </div>
               <div class="team-member">
                 <img src={BlenKedir} />
                 <p class="name">Blen Kedir</p>
+                <p class="school">Stanford University, 2023</p>
                 <p class="desc">Associate Director</p>
               </div>
             </div>
@@ -101,16 +113,19 @@ const Team = () => {
               <div class="team-member">
                 <img src={SandraChiu} />
                 <p class="name">Sandra Chiu</p>
+                <p class="school">Northwestern, 2023</p>
                 <p class="desc">Director of Media and Design</p>
               </div>
               <div class="team-member">
                 <img src={JeffreyYao} />
                 <p class="name">Jeffrey Yao</p>
+                <p class="school">Rhode Island School of Design, 2022</p>
                 <p class="desc">Associate Director of Media and Design</p>
               </div>
               <div class="team-member">
                 <img src={ChaseSouder} />
                 <p class="name">Chase Souder</p>
+                <p class="school">Harvard University, 2023</p>
                 <p class="desc">Co-Director of Business</p>
               </div>
             </div>
@@ -119,6 +134,7 @@ const Team = () => {
               <div class="team-member">
                 <img src={CarterMartindale} />
                 <p class="name">Carter Martindale</p>
+                <p class="school">Harvard University, 2023</p>
                 <p class="desc">Co-Director of Business</p>
               </div>
             </div>
