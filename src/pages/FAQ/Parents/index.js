e2bf82b58fbbea412c58e008719907ef0434fe48
dshuman12 <30298537+dshuman12@users.noreply.github.com>
2020-05-13 02:23:13 -0400

    Fixes Build error

--- src/pages/FAQ/Parents/index.js
+++ src/pages/FAQ/Parents/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from '../../components/Navbar'
-import Footer from '../../components/Footer'
+import Navbar from '../../../components/Navbar'
+import Footer from '../../../components/Footer'
 import { Container, ContainerInner } from './styles'
 
 const FAQStudents = () => {
