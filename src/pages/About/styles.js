0b48a0cec5908e42397cfb88492b67c799e9d191
mpekala23 <mpekala@college.harvard.edu>
2020-05-23 12:11:28 -0400

    Top section of new site almost done

--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
+import {Colors} from '../../styles'
 import { device } from '../../theme'
+
 export const Container = styled.div`
     width: 100%; 
     height: 65vh;
@@ -61,28 +63,38 @@ export const ContainerInner = styled.div`
 
 export const HeaderImage = styled.img`
     @media ${device.mobileS} {
-        width: 60%; 
+        width: 55%; 
         margin-top: 4em; 
     }
     @media ${device.tablet} {
-        width: 45%; 
+        width: 40%; 
         margin-top: 4em; 
     }
     @media ${device.tabletL} {
-        width: 45%; 
+        width: 40%; 
         margin-top: 0;  
     }
 
     @media ${device.laptop} {
-        width: 45%; 
+        width: 35%; 
         margin-top: 0;  
     }
      
     height: auto; 
 `
 
-export const AboutDescription = styled.div` 
+export const Highlight = styled.img`
+    position: absolute;
+    width: 150px;
+    height: 800px;
+    top: -165px;
+    left: 240px;
+    z-index: 1;
+`
 
+export const AboutDescription = styled.div` 
+    position: relative;
+    z-index: 2;
     @media ${device.mobileS} {
         width: 80%;
         display: flex; 
@@ -107,6 +119,7 @@ export const AboutDescription = styled.div`
 export const Header = styled.h1`
     color: #2684ff;
     font-size: 48px; 
+    font-family: 'Mukta Mahee', sans-serif;
 `
 
 export const DescriptionText = styled.p`
@@ -116,17 +129,17 @@ export const DescriptionText = styled.p`
 
 export const Button = styled.div`
     display: flex; 
-    background-color: #2684ff;
+    background-color: ${Colors.WLF_ORANGE};
     color: white; 
-    padding: 15px 30px 15px 30px;
+    padding: 5px 25px 5px 25px;
+    margin-right: 20px;
     align-items: center; 
     width: fit-content; 
-    border-radius: 50px; 
+    border-radius: 10px; 
 
     & p{
-        margin: 0px; 
-        margin-left: 10px;
-        font-size: 1.2em; 
+        margin: 0px;
+        font-size: 0.9em; 
     }
 `
 
@@ -324,101 +337,6 @@ export const WhoHeader = styled.div`
     }
 `
 
-export const NewsLetter = styled.div`
-    @media ${device.mobileS} {
-        width: 80%;
-    }
-    @media ${device.tablet} {
-        
-        width: 40%;
-    }
-    display: flex; 
-    flex-direction: column;     
-
-    & h2{
-        color: #2684ff;
-    }
-`
-export const Input = styled.input`
-    width: 100%; 
-    background-color: #fff;
-    padding: 0.75rem 1.25rem; 
-    font-size: 1rem;
-    font-weight: 400; 
-    line-height: 1.5; 
-    color: #8492a6;
-    transition: 0.2s; 
-    box-shadow: none;
-    border: 1px solid #e0e6ed; 
-    border-radius: .25rem; 
-
-    &:focus{
-        outline: 0; 
-        border-color: rgba(38, 132, 255, 0.7); 
-        box-shadow: 0 0 1.25rem rgba(31,45,61,.08); 
-    }
-`
-
-export const Subscribe = styled.button`
-    border: none;
-    
-    background-color: #2684ff;
-    color: white; 
-    padding: 12px 30px 12px 30px;
-    border-radius: 50px; 
-    font-size: 1rem; 
-    width: 100%; 
-    text-align: center; 
-    margin-top: 1rem; 
-    margin-bottom: 1rem;
-    transition: 0.15s;  
-
-    &:hover{
-        opacity: 0.8; 
-        cursor: pointer;
-    }
-
-    &:focus{
-        outline: none;
-    }
-
-    &:disabled{
-        cursor: default; 
-        opacity: 0.8; 
-    }
-`
-
-export const Popup = styled.div`
-    background-color: #2684ff; 
-    padding: 20px; 
-    box-shadow: 0 0 1.25rem rgba(31,45,61,.3); 
-    border-radius: 5px; 
-    position: relative; 
-    margin-bottom: 2rem; 
-	transition: all 1s cubic-bezier(.25,.8,.25,1);
-    opacity: ${props => props.subscribed ? '1' : '0'}; 
-
-
-    & p{
-        margin: 0px; 
-        color: #fff;
-    }
-`
-
-export const Triangle = styled.div`
-      position: absolute; 
-      bottom: 30px;
-      right: -40px ;
-      width: 0px;
-      height: 0px;
-      border-left: 50px solid #2684ff;
-      border-top: 30px solid transparent;
-      border-bottom: 30px solid transparent;
-      border-radius: 15px; 
-`
-
-export const Error = styled.p`
-    color: #ff5630; 
-    font-size: 0.9rem; 
-    margin: 0px; 
+export const SubHeaderContainer = styled.div`
+    width: 85%;
 `
