d15914867a35a9784050b1c3ac4eb0ca25fdc8ce
dshuman12 <30298537+dshuman12@users.noreply.github.com>
2020-06-01 01:27:39 -0400

    Set up adding courses environment

---
+++ src/pages/Courses_W2/index.js
@@ -0,0 +1,199 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
+import { Container, ContainerInner } from "../../globalStyles"
+import './styles.css'
+
+import LearnCancer from "./headshot/how-cancer-works.jpg"
+import LearnPython from "./headshot/learn-python.png"
+import Anthropology from "./headshot/anthropology.png"
+import BlockCoding from "./headshot/block-coding.png"
+import Emotions from "./headshot/emotions.png"
+import Explosives from "./headshot/explosives.png"
+import Geobiology from "./headshot/geobiology-astrobiology.png"
+import PublicTransit from "./headshot/public-transit.png"
+import ShortStories from "./headshot/short-stories.png"
+import SignLanguage from "./headshot/sign-language.png"
+import WesternMusic from "./headshot/western-music.png"
+
+const Courses = () => {
+    return(
+        <div>
+            <Navbar/>
+            <Container>
+            <ContainerInner>
+            <p>
+              For our inaugural wave, we are excited to offer 11 courses across a variety of subjects. Our volunteer educators have worked hard to prepare engaging and thoughtful curricula, and can't wait to share their passions with you. Click each course for more info, and feel free to send any questions to <a href="mailto:wavelf.logistics@gmail.com">wavelf.logistics@gmail.com</a>.
+            </p>
+            <div class="container">
+            <div class="row">
+            <div class="column">
+              <a href="course-how-cancer-works">
+                <div class="course">
+                  <div class="image-container">
+                    <img src={LearnCancer}/>
+                  </div>
+                  <p class="name">How Cancer Works</p>
+                  <p class="teacher">Madison Abbasi</p>
+                  <p class="school">Palo Alto High School ‘22</p>
+                </div>
+              </a>
+            </div>
+            <div class="column">
+              <a href="course-learn-python">
+                <div class="course">
+                  <div class="image-container">
+                    <img src={LearnPython} />
+                  </div>
+                  <p class="name">Learn Python</p>
+                  <p class="teacher">Fatima-Zahra Chriha</p>
+                  <p class="school">Felician University ‘23</p>
+                </div>
+              </a>
+            </div>
+            <div class="column">
+              <a href="course-short-stories">
+                <div class="course">
+                  <div class="image-container">
+                    <img src={ShortStories} />
+                  </div>
+                  <p class="name">Starting Short Stories</p>
+                  <p class="teacher">Annetta Venford</p>
+                  <p class="school">Henry M Gunn High School '21</p>
+                </div>
+              </a>
+            </div>
+            <div class="column">
+              <a href="course-anthropology">
+                <div class="course">
+                  <div class="image-container">
+                    <img src={Anthropology} />
+                  </div>
+                  <p class="name">What Even is Anthropology?</p>
+                  <p class="teacher">Cecelia Eure</p>
+                  <p class="school">College of William & Mary '23</p>
+                </div>
+              </a>
+            </div>
+            <div class="column">
+              <a href="course-sign-language">
+                <div class="course">
+                  <div class="image-container">
+                    <img src={SignLanguage} />
+                  </div>
+                  <p class="name">Introduction to American Sign Language</p>
+                  <p class="teacher">Emma Doughty</p>
+                  <p class="school">University at Buffalo '24</p>
+                </div>
+              </a>
+            </div>
+            <div class="column">
+              <a href="course-geobiology-astrobiology">
+                <div class="course">
+                  <div class="image-container">
+                    <img src={Geobiology} />
+                  </div>
+                  <p class="name">Geobiology and Astrobiology</p>
+                  <p class="teacher">Juliana Drozd</p>
+                  <p class="school">Massachusetts Institute of Technology '22</p>
+                </div>
+              </a>
+            </div>
+            <div class="column">
+              <a href="course-western-music">
+                <div class="course">
+                  <div class="image-container">
+                    <img src={WesternMusic} />
+                  </div>
+                  <p class="name">History’s Top Hits: An Introduction to the Great Works of Western Music</p>
+                  <p class="teacher">Justin Oei</p>
+                  <p class="school">College of William and Mary '23</p>
+                </div>
+              </a>
+            </div>
+            <div class="column">
+              <a href="course-explosives">
+                <div class="course">
+                  <div class="image-container">
+                    <img src={Explosives} />
+                  </div>
+                  <p class="name">The Chemistry of Explosives</p>
+                  <p class="teacher">Kristen Kiley</p>
+                  <p class="school">Harvard College '23</p>
+                </div>
+              </a>
+            </div>
+            <div class="column">
+              <a href="course-public-transportation">
+                <div class="course">
+                  <div class="image-container">
+                    <img src={PublicTransit} />
+                  </div>
+                  <p class="name">Public Transportation and Urban Planning</p>
+                  <p class="teacher">Perrin Price</p>
+                  <p class="school">Harvard College '23</p>
+                </div>
+              </a>
+            </div>
+            {/*
+            <div class="column">
+              <a href="course-web-development">
+                <div class="course">
+                  <div class="image-container">
+                    <img src={WebDevelopment} />
+                  </div>
+                  <p class="name">Introduction to Website Development</p>
+                  <p class="teacher">Ryan Cottone and Zachary Hauch</p>
+                </div>
+              </a>
+            </div>
+            */}
+            <div class="column">
+              <a href="course-emotions">
+                <div class="course">
+                  <div class="image-container">
+                    <img src={Emotions} />
+                  </div>
+                  <p class="name">Philosophy and Morality of Emotions</p>
+                  <p class="teacher">Sophia Giegerich</p>
+                  <p class="school">Whitman College '22</p>
+                </div>
+              </a>
+            </div>
+            <div class="column">
+              <a href="course-block-coding">
+                <div class="course">
+                  <div class="image-container">
+                    <img src={BlockCoding} />
+                  </div>
+                  <p class="name">How to Build an App with Block Coding</p>
+                  <p class="teacher">Yuan Sui</p>
+                </div>
+              </a>
+            </div>
+        </div>
+        </div>
+        <h1>Course Schedule</h1>
+          <iframe
+            src="https://calendar.google.com/calendar/embed?src=8tk6cntof4tuog58lv572ikcp4%40group.calendar.google.com&ctz=America%2FBoston"
+            style={{'border': '0px', 'width':'100%', 'height':'600px', 'frameborder':'0px', 'scrolling':'no'}}>
+          </iframe>
+        <h1>Register by 5/23 to take a class in the first wave!</h1>
+	<p>Sign-ups are on a first-come-first-serve basis. If you are unavailable for this wave, sign up for <a href = "www.wavelf.org/#newsletter">updates</a> to be the first to register for future waves!</p>
+            <iframe
+              title="form"
+              src="https://docs.google.com/forms/d/e/1FAIpQLSdEci1eOpQ8IvYSFCxsgQOXfKL5LpJhZRWvfBLrrzAPrgyuZw/viewform?embedded=true"
+              width="100%"
+              height="500"
+              frameborder="0"
+              marginheight="0"
+              marginwidth="0">Loading…
+            </iframe>
+            </ContainerInner>
+          </Container>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Courses
