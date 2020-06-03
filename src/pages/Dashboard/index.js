bdf8fb182aecf95ed940162855109454eccaa292
Billy Billy <eptgen@gmail.com>
2020-06-03 00:25:07 -0700

    added profile info

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -9,11 +9,19 @@ const Dashboard = () => {
         <div>
             <Navbar/>
 
-
+            <h1>Profile</h1>
+            <p><a href="/dashboard/edit-profile">Edit</a></p>
+            <p><b>Name: </b>Jonny Rogers</p>
+            <p><b>Email: </b>jsr7@williams.edu</p>
+            <p><b>Phone: </b>6502836176</p>
+            <p><b>Parent's Email: </b>parentemail@parentdomain.com</p>
+            <p><b>Parent's Phone: </b>6502983233</p>
+            <p><b>School: </b>Williams College</p>
+            <p><b>Location: </b>123 Main Street, United States, Palo Alto, CA 94306</p>
 
             <Footer/>
         </div>
     )
 }
 
-export default Courses
+export default Dashboard
