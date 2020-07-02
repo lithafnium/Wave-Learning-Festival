e0648cd9b9921dc766fefc26c5fafd9fa095f0d7
joshualchen <joshua.l.chen@gmail.com>
2020-07-02 13:02:21 -0700

    change wording action

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -318,7 +318,7 @@ const About = () => {
           >
             {" "}
             Sign up here to get alerted and be the first to register for Wave 4!
-            <Popup subscribed = {true}><p>Registration is currently closed. Registration for the new wave begins July 14--sign up and we will alert you as soon as courses open! Once you've signed up, click <a href="http://www.wavelf.org/courses">here</a> to see what we plan to offer!</p></Popup>
+            <Popup subscribed = {true}><p>Registration is currently closed. Registration for the new wave begins July 14--sign up and we will alert you as soon as courses open! Once you've signed up, click <a href="http://www.wavelf.org/courses">here</a> to see the courses we plan to offer!</p></Popup>
             {subscribed && <Popup subscribed={subscribed}><p>Thank you for subscribing! Please follow the directions sent to your email to ensure you receive our updates. Furthermore, click on "Courses" at the top of our webpage to check out the courses we'll be offering!</p>
                 </Popup>}
           </Typography.Header>
