0c8e53cea24f2611b35028990a9b046bcace2e16
Billy Billy <eptgen@gmail.com>
2020-07-03 15:38:45 -0700

    doesn't work :(

--- src/pages/Courses/CourseSignUp/index.js
+++ src/pages/Courses/CourseSignUp/index.js
@@ -4,12 +4,19 @@ import Footer from "@/components/Footer";
 import { Colors, Typography, Form } from "@/styles";
 import * as Styles from "./styles";
 import Logo from "./logo.png"
+import firebase from 'firebase'
 import { FirebaseContext } from "@/firebaseContext";
 
-const Home = (setPage) => {
+const Home = (setPage, student) => {
   return (
-    <div>
-    </div>
+    <>
+    <Typography.Header color={Colors.WLF_YELLOW}>
+      Course Signup
+    </Typography.Header>
+    <Typography.BodyText>
+      Currently signed up as {student.name}.
+    </Typography.BodyText>
+    </>
   );
 }
 
@@ -20,40 +27,48 @@ const Complete = () => {
   );
 }
 
+const Loading = () => {
+  return (
+    <Typography.Header color={Colors.WLF_YELLOW}>Loading...</Typography.Header>
+  );
+}
+
+const Error = () => {
+  return (
+    <Typography.Header color={Colors.WLF_YELLOW}>Error!</Typography.Header>
+  )
+}
+
 const CourseSignUp = () => {
-  const [page, setPage] = useState("home");
-  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState("loading");
+  const [user, setUser] = useState(null);
+  const [calledOnce, setCalledOnce] = useState(false);
+  const [student, setStudent] = useState(null);
 
-  if (loading) {
-    return (
+  const {db} = useContext(FirebaseContext);
 
-        <div style={{overflow: 'hidden', position: 'relative'}}>
-          <Navbar />
-          <Styles.SignupBackground>
-            <div style={{maxWidth: 800}}>
-              {page === "home" && Home(setPage)}
-              {page === "complete" && Complete()}
-            </div>
-          </Styles.SignupBackground>
-          <Styles.LogoBackground src={Logo} alt="logo" style={{
-            position: 'absolute',
-            width: 300,
-            height: 300,
-            transform: 'rotate(-35deg)',
-            top: '60%',
-            left: -100,
-          }}/>
-          <Styles.LogoBackground src={Logo} alt="logo" style={{
-            position: 'absolute',
-            width: 300,
-            height: 300,
-            transform: 'rotate(-235deg)',
-            top: '20%',
-            right: -150,
-          }}/>
-          <Footer />
-        </div>
-    )
+  if (db && !calledOnce) {
+    setCalledOnce(true);
+    firebase.auth().signInWithEmailAndPassword("jsr7@williams.edu", "cheesemaker123").catch(function(error) { // DELETE THIS LATER
+      setPage("error");
+      console.log("nuh");
+    }).then(function(result) {
+      if (result) {
+        console.log("yuh");
+        setUser(result.user);
+        db.collection("students2").where("userID", "==", result.user.uid).get().then(function(snapshot) {
+          var students = [];
+          snapshot.forEach(function(snap) {
+            students.push(snap);
+          });
+          if (students.length > 0) {
+            setStudent(students[0].data());
+            console.log(students[0].data().email);
+            setPage("home");
+          }
+        });
+      }
+    });
   }
 
   return (
@@ -61,8 +76,10 @@ const CourseSignUp = () => {
       <Navbar />
       <Styles.SignupBackground>
         <div style={{maxWidth: 800}}>
-          {page === "home" && Home(setPage)}
+          {page === "home" && Home(setPage, student)}
           {page === "complete" && Complete()}
+          {page === "loading" && Loading()}
+          {page === "error" && Error()}
         </div>
       </Styles.SignupBackground>
       <Styles.LogoBackground src={Logo} alt="logo" style={{
