b02124fa8f30330cc4c433e534d2532d53da67cd
Steve Li <lithafnium2000@gmail.com>
2020-07-17 19:53:55 -0400

    added featured speaker info and started on adding others

--- src/pages/Speakers/SpeakerCard/index.js
+++ src/pages/Speakers/SpeakerCard/index.js
@@ -1,22 +1,15 @@
 import React from 'react'
-
-import Nate from '../nate_saal.jpg'
-import { SpeakerContainer, SpeakerImage, Description, SpeakerContainerInner, Button } from './styles'
-const SpeakerCard = ({ picture, description }) => {
+import { Card, Speaker, Info, Header, Details } from './styles'
+import Andrew from './andrew_cramer.jpg'
+const SpeakerCard = () => {
   return (
-    <SpeakerContainer>
-      <h1>Nate Saal: The Art and Science of Making Chocolate</h1>
-      <p>CEO, CocoTerra Company</p>
-      <SpeakerContainerInner>
-        <SpeakerImage src={Nate} />
-        <div>
-          <Description>
-        I am a San Francisco Bay Area native with a passion for technology and food. For the last 25 years, I’ve been in the start-up world as a founder or part of the founding team of multiple software, middleware and hardware companies.  With academic training in molecular biophysics and biochemistry, I've had a lifelong interest in technology, food science and DIY food making.  Professionally, I've spent my career as a serial entrepreneur bringing new technologies to market.  When I'm not hard at work, you'll find me beekeeping, making wine, olive oil and vinegar, growing exotic fruits, and a host of weekend farming activities.  My latest venture, CocoTerra, sits at the intersection of technology and food.  It draws on my knowledge and skills from all my different business experiences to simplify the chocolate-making process using smart software and hardware design so anyone can become a creative, successful and proud chocolate-maker.  Go to www.cocoterra.com if you are curious to learn more.
-          </Description>
-          <Button>RSVP Now</Button>
-        </div>
-      </SpeakerContainerInner>
-    </SpeakerContainer>
+    <Card>
+      <Speaker src={Andrew}/>
+      <Info>
+        <Header>Andy Cramer</Header>
+        <Details>Professor, Los Angeles Mission College</Details>
+      </Info>
+    </Card>
   )
 }
 
--- src/pages/Speakers/index.js
+++ src/pages/Speakers/index.js
@@ -2,16 +2,26 @@ import React from 'react'
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
 import { Container, ContainerInner } from '../../globalStyles'
-import SpeakerCard from './SpeakerCard'
+import FeaturedSpeaker from './FeaturedSpeaker'
 import { Featured } from './styles'
+import AnimateHeight from 'react-animate-height'
+import SpeakerCard from './SpeakerCard'
 
 const Speakers = () => {
   return (
     <div>
       <Navbar/>
-      <Container>
+      <Container style={{ minHeight: '0vh' }}>
         <ContainerInner>
           <Featured>Featured Speaker : </Featured>
+          <FeaturedSpeaker/>
+        </ContainerInner>
+      </Container>
+      <Container>
+        <ContainerInner>
+          <Featured>Upcoming Speakers : </Featured>
+          <SpeakerCard/>
+          <SpeakerCard/>
           <SpeakerCard/>
         </ContainerInner>
       </Container>
