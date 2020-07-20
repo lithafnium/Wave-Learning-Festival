43dcb064daaf85c18168eee3538cbc981efb8c90
Steve Li <lithafnium2000@gmail.com>
2020-07-20 13:17:29 -0400

    added melina

--- src/pages/Team/index.js
+++ src/pages/Team/index.js
@@ -1,187 +1,195 @@
-import React from "react";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import React from 'react'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
-import { Colors, Typography } from "@/styles";
-import * as Styles from "./styles";
+import { Colors, Typography } from '@/styles'
+import * as Styles from './styles'
 
-import BlenKedir from "./headshot/blen-beharu-kedir.png";
-import CarterMartindale from "./headshot/carter-martindale.jpg";
-import ChaseSouder from "./headshot/chase-souder.JPG";
-import DanielaShuman from "./headshot/daniela-shuman.png";
-import IrisSu from "./headshot/iris-su.jpg";
-import JeffreyYao from "./headshot/jeffrey-yao.jpg";
-import JonnyRogers from "./headshot/jonny-rogers.jpg";
-import KarlyHou from "./headshot/karly-hou.JPG";
-import KevinTan from "./headshot/kevin-tan.jpg";
-import NikkiSuzani from "./headshot/nikki-suzani.JPG";
-import RuhiMistry from "./headshot/ruhi-mistry.JPG";
-import SalmaKamni from "./headshot/salma-kamni.jpeg";
-import SandraChiu from "./headshot/sandra-chiu.png";
-import SimonTao from "./headshot/simon-tao.jpg";
-import SteveLi from "./headshot/steve-li.jpg";
-import YaelLivneh from "./headshot/yael-livneh.jpg";
-import JoshuaChen from "./headshot/joshua-chen.png";
-import AustinLi from "./headshot/austin-li.jpeg";
-import MarkPekala from "./headshot/mark-pekala.jpg";
-import AlainDalma from "./headshot/alain-saal-dalma.jpg";
-import AmyShen from "./headshot/amy-shen.jpg";
+import BlenKedir from './headshot/blen-beharu-kedir.png'
+import CarterMartindale from './headshot/carter-martindale.jpg'
+import ChaseSouder from './headshot/chase-souder.JPG'
+import DanielaShuman from './headshot/daniela-shuman.png'
+import IrisSu from './headshot/iris-su.jpg'
+import JeffreyYao from './headshot/jeffrey-yao.jpg'
+import JonnyRogers from './headshot/jonny-rogers.jpg'
+import KarlyHou from './headshot/karly-hou.JPG'
+import KevinTan from './headshot/kevin-tan.jpg'
+import NikkiSuzani from './headshot/nikki-suzani.JPG'
+import RuhiMistry from './headshot/ruhi-mistry.JPG'
+import SalmaKamni from './headshot/salma-kamni.jpeg'
+import SandraChiu from './headshot/sandra-chiu.png'
+import SimonTao from './headshot/simon-tao.jpg'
+import SteveLi from './headshot/steve-li.jpg'
+import YaelLivneh from './headshot/yael-livneh.jpg'
+import JoshuaChen from './headshot/joshua-chen.png'
+import AustinLi from './headshot/austin-li.jpeg'
+import MarkPekala from './headshot/mark-pekala.jpg'
+import AlainDalma from './headshot/alain-saal-dalma.jpg'
+import AmyShen from './headshot/amy-shen.jpg'
+import MelinaMuthuswamy from './headshot/melina-muthuswamy.jpg'
 
-const CHAIR = "Chair";
-const DIRECTOR_LOGISTICS = "Director of Operations";
-const ASSOC_DIRECTOR_LOGISTICS = "Associate Director of Operations";
-const CO_DIRECTOR_TECH = "Co-Director of Tech";
-const ASSOC_DIRECTOR_TECH = "Associate Director of Tech";
-const DIRECTOR_PR = "Director of PR";
-const ASSOC_DIRECTOR_PR = "Associate Director of PR";
-const DIRECTOR_MARKETING = "Director of Marketing";
-const DIRECTOR_MEDIA_DESIGN = "Director of Media and Design";
-const ASSOC_DIRECTOR_MEDIA_DESIGN = "Associate Director of Media and Design";
-const DIRECTOR_BUSINESS = "Director of Business";
-const ADVISOR = "Advisor";
+const CHAIR = 'Chair'
+const DIRECTOR_LOGISTICS = 'Director of Operations'
+const ASSOC_DIRECTOR_LOGISTICS = 'Associate Director of Operations'
+const CO_DIRECTOR_TECH = 'Co-Director of Tech'
+const ASSOC_DIRECTOR_TECH = 'Associate Director of Tech'
+const DIRECTOR_PR = 'Director of PR'
+const ASSOC_DIRECTOR_PR = 'Associate Director of PR'
+const DIRECTOR_MARKETING = 'Director of Marketing'
+const ASSOC_DIRECTOR_MARKETING = 'Associate Director of Marketing'
+const DIRECTOR_MEDIA_DESIGN = 'Director of Media and Design'
+const ASSOC_DIRECTOR_MEDIA_DESIGN = 'Associate Director of Media and Design'
+const DIRECTOR_BUSINESS = 'Director of Business'
+const ADVISOR = 'Advisor'
 
 const teamData = [
   {
-    name: "Karly Hou",
-    school: "Harvard 2023",
+    name: 'Karly Hou',
+    school: 'Harvard 2023',
     position: CHAIR,
-    image: KarlyHou,
+    image: KarlyHou
   },
   {
-    name: "Amy Shen",
-    school: "University of Pennsylvania 2022",
+    name: 'Amy Shen',
+    school: 'University of Pennsylvania 2022',
     position: DIRECTOR_LOGISTICS,
-    image: AmyShen,
+    image: AmyShen
   },
   {
-    name: "Salma Kamni",
-    school: "Stanford 2024",
+    name: 'Salma Kamni',
+    school: 'Stanford 2024',
     position: ASSOC_DIRECTOR_LOGISTICS,
-    image: SalmaKamni,
+    image: SalmaKamni
   },
   {
-    name: "Simon Tao",
-    school: "Stanford 2022",
+    name: 'Simon Tao',
+    school: 'Stanford 2022',
     position: ASSOC_DIRECTOR_LOGISTICS,
-    image: SimonTao,
+    image: SimonTao
   },
   {
-    name: "Kevin Tan",
-    school: "Harvard 2023",
+    name: 'Kevin Tan',
+    school: 'Harvard 2023',
     position: ASSOC_DIRECTOR_LOGISTICS,
-    image: KevinTan,
+    image: KevinTan
   },
   {
-    name: "Austin Li",
-    school: "Harvard 2023",
+    name: 'Austin Li',
+    school: 'Harvard 2023',
     position: ASSOC_DIRECTOR_LOGISTICS,
-    image: AustinLi,
+    image: AustinLi
   },
   {
-    name: "Alain Saal-Dalma",
-    school: "Carnegie Mellon University 2023",
+    name: 'Alain Saal-Dalma',
+    school: 'Carnegie Mellon University 2023',
     position: ASSOC_DIRECTOR_LOGISTICS,
-    image: AlainDalma,
+    image: AlainDalma
   },
   {
-    name: "Steve Li",
-    school: "Harvard 2023",
+    name: 'Steve Li',
+    school: 'Harvard 2023',
     position: CO_DIRECTOR_TECH,
-    image: SteveLi,
+    image: SteveLi
   },
   {
-    name: "Daniela Shuman",
-    school: "Harvard 2023",
+    name: 'Daniela Shuman',
+    school: 'Harvard 2023',
     position: CO_DIRECTOR_TECH,
-    image: DanielaShuman,
+    image: DanielaShuman
   },
   {
-    name: "Jonny Rogers",
-    school: "Williams College 2023",
+    name: 'Jonny Rogers',
+    school: 'Williams College 2023',
     position: ASSOC_DIRECTOR_TECH,
-    image: JonnyRogers,
+    image: JonnyRogers
   },
   {
-    name: "Mark Pekala",
-    school: "Harvard 2023",
+    name: 'Mark Pekala',
+    school: 'Harvard 2023',
     position: ASSOC_DIRECTOR_TECH,
-    image: MarkPekala,
+    image: MarkPekala
   },
   {
-    name: "Iris Su",
-    school: "Harvard 2023",
+    name: 'Iris Su',
+    school: 'Harvard 2023',
     position: DIRECTOR_PR,
-    image: IrisSu,
+    image: IrisSu
   },
   {
-    name: "Ruhi Mistry",
-    school: "UC Santa Cruz 2024",
+    name: 'Ruhi Mistry',
+    school: 'UC Santa Cruz 2024',
     position: ASSOC_DIRECTOR_PR,
-    image: RuhiMistry,
+    image: RuhiMistry
   },
   {
-    name: "Nikki Suzani",
-    school: "Henry M. Gunn High School 2021",
+    name: 'Nikki Suzani',
+    school: 'Henry M. Gunn High School 2021',
     position: ASSOC_DIRECTOR_PR,
-    image: NikkiSuzani,
+    image: NikkiSuzani
   },
   {
-    name: "Blen Kedir",
-    school: "Stanford 2023",
+    name: 'Blen Kedir',
+    school: 'Stanford 2023',
     position: ASSOC_DIRECTOR_PR,
-    image: BlenKedir,
+    image: BlenKedir
   },
   {
-    name: "Joshua Chen",
-    school: "University of Pennsylvania 2022",
+    name: 'Joshua Chen',
+    school: 'University of Pennsylvania 2022',
     position: DIRECTOR_MARKETING,
-    image: JoshuaChen,
+    image: JoshuaChen
   },
   {
-    name: "Sandra Chiu",
-    school: "Northwestern 2023",
+    name: 'Melina Muthuswamy',
+    school: 'University of Pennsylvania 2022',
+    position: ASSOC_DIRECTOR_MARKETING,
+    image: MelinaMuthuswamy
+  },
+  {
+    name: 'Sandra Chiu',
+    school: 'Northwestern 2023',
     position: DIRECTOR_MEDIA_DESIGN,
-    image: SandraChiu,
+    image: SandraChiu
   },
   {
-    name: "Jeffrey Yao",
-    school: "Rhode Island School of Design 2022",
+    name: 'Jeffrey Yao',
+    school: 'Rhode Island School of Design 2022',
     position: ASSOC_DIRECTOR_MEDIA_DESIGN,
-    image: JeffreyYao,
+    image: JeffreyYao
   },
   {
-    name: "Carter Martindale",
-    school: "Harvard 2023",
+    name: 'Carter Martindale',
+    school: 'Harvard 2023',
     position: DIRECTOR_BUSINESS,
-    image: CarterMartindale,
+    image: CarterMartindale
   },
   {
-    name: "Yael Livneh",
-    school: "Cal Poly SLO 2023",
+    name: 'Yael Livneh',
+    school: 'Cal Poly SLO 2023',
     position: ADVISOR,
-    image: YaelLivneh,
-  },
+    image: YaelLivneh
+  }
 ]
 
-function getColor(position) {
+function getColor (position) {
   if (position === CHAIR || position === DIRECTOR_MARKETING) {
-    return Colors.WLF_PURPLE;
+    return Colors.WLF_PURPLE
   } else if (position === DIRECTOR_LOGISTICS || position === ASSOC_DIRECTOR_LOGISTICS || position === DIRECTOR_MEDIA_DESIGN || position === ASSOC_DIRECTOR_MEDIA_DESIGN) {
-    return Colors.WLF_TURQOUISE;
+    return Colors.WLF_TURQOUISE
   } else if (position === CO_DIRECTOR_TECH || position === ASSOC_DIRECTOR_TECH || position === DIRECTOR_BUSINESS) {
-    return Colors.WLF_YELLOW;
+    return Colors.WLF_YELLOW
   } else if (position === DIRECTOR_PR || position === ASSOC_DIRECTOR_PR) {
-    return Colors.WLF_ORANGE;
+    return Colors.WLF_ORANGE
   } else {
     return Colors.WLF_TURQOUISE
   }
 }
 
 const PersonIcon = (person) => {
-  if (person.name === "blank") return <div />
+  if (person.name === 'blank') return <div />
   return (
     <Styles.PersonBackground>
-      <Styles.PersonImage src={person.image} style={{borderColor: getColor(person.position)}}/>
+      <Styles.PersonImage src={person.image} style={{ borderColor: getColor(person.position) }}/>
       <Typography.Name>{person.name}</Typography.Name>
       <Typography.School>{person.school}</Typography.School>
       <Typography.Position>{person.position}</Typography.Position>
@@ -190,17 +198,17 @@ const PersonIcon = (person) => {
 }
 
 const Team = () => {
-    return(
-      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-        <Navbar/>
-        <Styles.TeamRow>
-          {teamData.map((person) => {
-            return PersonIcon(person);
-          })}
-        </Styles.TeamRow>
-        <Footer />
-      </div>
-  );
-};
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Navbar/>
+      <Styles.TeamRow>
+        {teamData.map((person) => {
+          return PersonIcon(person)
+        })}
+      </Styles.TeamRow>
+      <Footer />
+    </div>
+  )
+}
 
-export default Team;
+export default Team
