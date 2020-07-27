3194be0b002e86c3e8eb9d44dcb02df8cb9defbe
Janet Li <janetli@janets-mbp-2.attlocal.net>
2020-07-27 15:25:29 -0700

    updated title of cliff seminar in rsvp form

--- src/pages/Speakers/SpeakerSignup/data.js
+++ src/pages/Speakers/SpeakerSignup/data.js
@@ -61,5 +61,5 @@ export const STATES = ['Alaska',
 export const SPEAKERS = [
   "Q&A with Jameela Jamil (7/30 7pm EDT)",
   'Nate Saal: The Art and Science of Chocolate Making (8/1 5pm EDT)', 
-  'Cliff Hsia: 3 Part Seminar'
+  "Cliff Hsia: How to Write a Resumé That Will Get Recruiters’ Attention and Get You Interviews (8/5 7pm EDT)"
 ]
