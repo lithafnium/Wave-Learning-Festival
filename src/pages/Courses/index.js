bdaab638f1185abcb567a35f9299e20edad61105
Steve Li <lithafnium2000@gmail.com>
2020-07-31 16:12:43 -0400

    changed course number

--- src/pages/Courses/index.js
+++ src/pages/Courses/index.js
@@ -208,7 +208,7 @@ const Courses = () => {
           </div>
 
           <Typography.BodyText style={{ color: Colors.WLF_BLACK }}>
-            We are excited to offer <strong>124 courses</strong> across a variety of subjects for Wave Four running from <strong>August 17th to August 30th</strong>. Our
+            We are excited to offer <strong>{courses.length} courses</strong> across a variety of subjects for Wave Four running from <strong>August 17th to August 30th</strong>. Our
             volunteer educators have worked hard to prepare engaging and
             thoughtful curricula and can't wait to share their passions with
             you. Feel free to send any
